9ad16083859be721962ea5369e30e75440c423ab
Gertjan Halkes <hg@ghalkes.nl>
2011-10-31 10:22:32 +0100

    Tested all languages for proper loading (added testsuite script for that)

--- testsuite/testdistlangs.sh
+++ testsuite/testdistlangs.sh
@@ -2,7 +2,7 @@
 
 DIR="`dirname \"$0\"`"
 
-for i in `hg manifest | egrep '^src/data' | egrep -v '^src/data/def' | sed -r "s%^%$DIR/../%"` ; do
+for i in `hg manifest | egrep '^src/data.*\.lang$' | egrep -v '^src/data/def' | sed -r "s%^%$DIR/../%"` ; do
 	echo "Testing file $i"
 	"$DIR/../src.util/run" --language-file="$i" "$0" > /dev/null
 done
