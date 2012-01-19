0d8b94fc2fa25e2e5fb190d01aa89928debde136
Gertjan Halkes <hg@ghalkes.nl>
2012-01-19 16:40:24 +0100

    Drop debug scripts from dist

--- dist_config.sh
+++ dist_config.sh
@@ -1,5 +1,5 @@
 PACKAGE=libt3highlight
 SRCDIRS="src src.util"
-EXCLUDESRC="/(Makefile|TODO.*|SciTE.*|run\.sh|test\.c)$"
+EXCLUDESRC="/(Makefile|TODO.*|SciTE.*|test\.c|run|valgrind|debug)$"
 GENSOURCES="`echo src/.objects/*.bytes src.util/.objects/*.bytes` src/highlight_api.h src/highlight_errors.h src/highlight_shared.c"
 VERSIONINFO="1:0:0"
