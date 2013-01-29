23850602903e9e7ad1198e9fcad70e97100d33c6
Gertjan Halkes <git@ghalkes.nl>
2013-01-29 22:24:45 +0100

    Converted .hgignore to .gitignore; converted mkdist.sh to new python based script; fix src.util Makefile

---
+++ dist_config.py
@@ -0,0 +1,47 @@
+import os
+
+package = 'libt3highlight'
+excludesrc = '/(Makefile|TODO.*|SciTE.*|test\.c|run|valgrind|debug)$'
+auxsources = [ 'src/.objects/*.bytes', 'src.util/.objects/*.bytes', 'src/highlight_api.h',
+	'src/highlight_errors.h', 'src/highlight_shared.c' ]
+auxfiles = [ 'doc/API' ]
+extrabuilddirs = [ 'doc' ]
+
+versioninfo = '1:0:0'
+
+def get_replacements(mkdist):
+	return [
+		{
+			'tag': '<VERSION>',
+			'replacement': mkdist.version
+		},
+		{
+			'tag': '^#define T3_HIGHLIGHT_VERSION .*',
+			'replacement': '#define T3_HIGHLIGHT_VERSION ' + mkdist.get_version_bin(),
+			'files': [ 'src/highlight.h' ],
+			'regex': True
+		},
+		{
+			'tag': '<OBJECTS>',
+			'replacement': " ".join(mkdist.sources_to_objects(mkdist.include_by_regex(mkdist.sources, '^src/'), '\.c$', '.lo')),
+			'files': [ 'mk/libt3highlight.in' ]
+		},
+		{
+			'tag': '<OBJECTS>',
+			'replacement': " ".join(mkdist.sources_to_objects(mkdist.include_by_regex(mkdist.sources, '^src\.util/'), '\.c$', '.o')),
+			'files': [ 'mk/t3highlight.in' ]
+		},
+		{
+			'tag': '<VERSIONINFO>',
+			'replacement': versioninfo,
+			'files': [ 'mk/libt3highlight.in' ]
+		},
+		{
+			'tag': '<LIBVERSION>',
+			'replacement': versioninfo.split(':', 2)[0],
+			'files': [ 'Makefile.in', 'mk/*.in' ]
+		}
+	]
+
+def finalize(mkdist):
+	os.symlink('.', os.path.join(mkdist.topdir, 't3highlight'))
